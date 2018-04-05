36cc8a562dc178d1bf766cbadbbe842d824d5538
elias <elias.jm.garcia@gmail.com>
2018-04-05 13:55:13 -0500

    WUTANG WUTANG

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+try:
+    with open('README.rst') as file:
+        long_description = file.read()
+except Exception as error:
+    print("Could not find README.rst for long description. Error: {}".format(
+        error))
+    print("Leaving long_description as None")
+    long_description = None
+
+setup(
+    name='wutangy',
+    author='elias julian marko garcia',
+    author_email='elias.jm.garcia@gmail.com',
+    description=(
+        'wutangy is a WuTang inspired lorem ispsum generator, SUUUUUUUUUU'),
+    url='https://github.com/ejmg/wutangy',
+    version='0.0.1dev',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ['wutang = wutang.wutangy:wu'],
+    },
+    install_requires=[],
+    include_package_data=True,
+    license='MIT',
+    long_description=long_description)
